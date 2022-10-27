d93e243fa220ef2cb98b4a4d8da27939ff030fea
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-10-27 10:25:42 +0300

    add finish validate form

--- src/js/form.js
+++ src/js/form.js
@@ -1,18 +1,16 @@
 import Inputmask from "inputmask";
 import JustValidate from "just-validate";
 
-const form = document.querySelector(".main-form");
 const formInputs = document.querySelectorAll(".main-form__input");
 const formInputPhone = document.querySelector(".main-form__input--phone");
 const formInputName = document.querySelector(".main-form__input--name");
-const formBtn = document.querySelector(".main-form__btn");
+const formInputFile = document.querySelector(".main-form__input--file");
+const content = document.querySelector(".main-content");
 
 const inputMask = new Inputmask.default("+(380) 999-999-999");
 inputMask.mask(formInputPhone);
 
-const validate = new JustValidate("#form", {
-  // errorFieldCssClass: "is-invalid",
-});
+const validate = new JustValidate("#form");
 
 validate
   .addField("#name", [
@@ -39,7 +37,6 @@ validate
       rule: "required",
     },
   ])
-
   .addField("#file", [
     {
       rule: "files",
@@ -54,79 +51,25 @@ validate
       rule: "minFilesCount",
       value: 1,
     },
-  ]);
+  ])
+  .onSuccess(() => {
+    const valuePhone = formInputPhone.value;
+    const valueName = formInputName.value;
+    const valueFile = URL.createObjectURL(formInputFile.files[0]);
 
-// formBtn.addEventListener("click", (event) => {
-//   event.preventDefault();
-// });
+    formInputs.forEach((input) => {
+      input.value = "";
+    });
 
-// const validate = new JustValidate("#form", {
-//   rules: {
-//     tel: {
-//       required: true,
-//       function: () => {
-//         const phone = formInputPhone.inputmask.unmaskedvalue();
-//         console.log(phone);
+    content.innerHTML += `<div class="main-content__body"><button class="main-content__btn" type="button">X</button><div class="main-content__text">${valueName}</div>
+    <div class="main-content__text">${valuePhone}</div>
+    <img class="main-content__img" src="${valueFile}" alt="file"></div>`;
 
-//         return Number(phone) && phone.length === 10;
-//       },
-//     },
-//   },
-// });
+    const closeBtnCards = document.querySelectorAll(".main-content__btn");
 
-// const phoneValue = formInputPhone.value;
-// const nameValue = formInputName.value;
-// const emptyInputs = Array.from(formInputs).filter(
-//   (input) => input.value === ""
-// );
-
-// formInputs.forEach(function (input) {
-//   if (input.value === "") {
-//     input.classList.add("error");
-//   } else {
-//     input.classList.remove("error");
-//   }
-// });
-
-//   if (emptyInputs.length !== 0) {
-//     console.log("inputs not filled!");
-//     return false;
-//   }
-
-// function validatePhone(phone) {
-//   // const regular = /^\+38\s\(\d{3 }\);
-//   return regular.test(String(phone));
-// }
-
-// function validateName(name) {
-//   const regular = [a - zA - Z] + $;
-//   return regular.test(String(name));
-// }
-
-// form.onsubmit = function () {
-//   const phoneValue = formInputPhone.value;
-//   const emptyInputs = Array.from(formInputs).filter(
-//     (input) => input.value === ""
-//   );
-
-//   formInputs.forEach(function (input) {
-//     if (input.value === "") {
-//       input.classList.add("error");
-//     } else {
-//       input.classList.remove("error");
-//     }
-//   });
-
-//   if (emptyInputs.length !== 0) {
-//     console.log("inputs not filled!");
-//     return false;
-//   }
-
-//   if (!validatePhone(phoneValue)) {
-//     console.log("phone not valid!");
-//     formInputPhone.classList.add("error");
-//     return false;
-//   } else {
-//     formInputPhone.classList.remove("error");
-//   }
-// };
+    closeBtnCards.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        btn.parentElement.remove();
+      });
+    });
+  });
