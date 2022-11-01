06e1812aaa67a6b5de9302a93d45c7ccc14d9c2f
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-11-01 11:48:46 +0200

    add fix all

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,56 +1,53 @@
-// ac3b4807bd5c5cf0b0ac4f15fd23d69a;
-// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
 // блок з погодою
-// const weatherBlock = document.querySelector(".weather");
+const weatherBlock = document.querySelector(".weather");
 
-// async function loadWeather() {
-//   weatherBlock.innerHTML = `
-// <div class="weather__loading">
-//   <img class="weather__img" src="img/loading.gif" alt="Loading...">
-// </div>`;
+async function loadWeather() {
+  weatherBlock.innerHTML = `
+<div class="weather__loading">
+  <img class="weather__img" src="img/loading.gif" alt="Loading...">
+</div>`;
 
-//   const server =
-//     "https://api.openweathermap.org/data/3.0/weather?units=metric&q=Kyiv&&appid=ac3b4807bd5c5cf0b0ac4f15fd23d69a";
-//   const response = await fetch(server, { method: "GET" });
-//   const responseResult = await response.json();
+  const server =
+    "https://api.openweathermap.org/data/3.0/weather?units=metric&q=Kyiv&&appid=ac3b4807bd5c5cf0b0ac4f15fd23d69a";
+  const response = await fetch(server, { method: "GET" });
+  const responseResult = await response.json();
 
-//   if (response.ok) {
-//     getWeather(responseResult);
-//   } else {
-//     weatherBlock.innerHTML = responseResult.message;
-//   }
-// }
+  if (response.ok) {
+    getWeather(responseResult);
+  } else {
+    weatherBlock.innerHTML = responseResult.message;
+  }
+}
 
-// function getWeather(data) {
-//   // обробляжмо і виводимо дані
-//   console.log(data);
+function getWeather(data) {
+  // обробляжмо і виводимо дані
+  console.log(data);
 
-//   const location = data.name;
-//   const temp = Math.round(data.main.temp);
-//   const feelsLike = Math.round(data.main.feels_like);
-//   const weatherStatus = data.weather[0].main;
-//   const weatherIcon = data.weather[0].icon;
+  const location = data.name;
+  const temp = Math.round(data.main.temp);
+  const feelsLike = Math.round(data.main.feels_like);
+  const weatherStatus = data.weather[0].main;
+  const weatherIcon = data.weather[0].icon;
 
-//   const template = `
-//   <div class="weather__header">
-//     <div class="weather__main">
-//       <div class="weather__city">${location}</div>
-//       <div class="weather__status">${weatherStatus}</div>
-//     </div>
-//     <div class="weather__icon">
-//         <img src="http://openweathermap.org/img/w/${weatherIcon}" alt="${weatherStatus}">
-//     </div>
-//   </div>
-//   <div class="weather__temp">${temp}</div>
-//   <div class="weather__feels-like">Feels like: ${feelsLike}</div>`;
+  const template = `
+  <div class="weather__header">
+    <div class="weather__main">
+      <div class="weather__city">${location}</div>
+      <div class="weather__status">${weatherStatus}</div>
+    </div>
+    <div class="weather__icon">
+        <img src="http://openweathermap.org/img/w/${weatherIcon}" alt="${weatherStatus}">
+    </div>
+  </div>
+  <div class="weather__temp">${temp}</div>
+  <div class="weather__feels-like">Feels like: ${feelsLike}</div>`;
 
-//   weatherBlock.innerHTML = template;
-// }
+  weatherBlock.innerHTML = template;
+}
 
-// if (weatherBlock) {
-//   loadWeather();
-// }
+if (weatherBlock) {
+  loadWeather();
+}
 
 // ++++++++++++++++++
 
@@ -60,11 +57,6 @@ const content = document.querySelector(".content__list");
 const buttonNext = document.querySelector(".content__btn");
 
 async function loadContent() {
-  // content.innerHTML = `
-  // <div class="content__loading">
-  //   <img class="content__img" src="img/loading.gif" alt="Loading...">
-  // </div>`;
-
   const server = "https://swapi.dev/api/people";
   const response = await fetch(server, {});
   const responseResult = await response.json();
@@ -77,10 +69,11 @@ async function loadContent() {
 }
 
 function getContent(data) {
+  console.log(data);
   data.results.forEach((person) => {
     console.log(person);
 
-    const template = `<div class="content__item">
+    const template = `<div class="content__item" >
   <div class="content__content">${person.name}</div>
    <div class="content__content">${person.height}</div>
     <div class="content__content">${person.mass}</div>
@@ -92,9 +85,5 @@ function getContent(data) {
     content.innerHTML += template;
   });
 }
-
-// if (content) {
-//   loadContent();
-// }
 
 loadContent();
